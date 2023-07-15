fa80fa7ef110a7aa7d106147d9327d8bffae17f3
Kevin Veen-Birkenbach <kevin@veen.world>
2023-07-15 15:58:26 +0200

    Optimized draft for watch schedule

--- generate.py
+++ generate.py
@@ -1,37 +1,53 @@
 import pandas as pd
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 class Schichtplaner:
     def __init__(self):
 
-        # Klassifizieren der Personen
-        self.expert = ["Dirk", "Thomas", "Kevin", "Detlef"]
-        self.springer = ["Detlef"]
-        self.unerfahren = ["Steve", "Lasse", "Stefan"]
+        self.crew   = [
+            {"name":"Dirk","experience":"10","watch_count":0},
+            {"name":"Thomas","experience":"8","watch_count":0},
+            {"name":"Kevin","experience":"8","watch_count":0},
+            {"name":"Detlef","experience":"6","watch_count":0},
+            {"name":"Steve","experience":"2","watch_count":0},
+            {"name":"Lasse","experience":"4","watch_count":0},
+            {"name":"Stefan","experience":"5","watch_count":0}
+        ]
 
         # Wachwechsel alle 3:30 Stunden, Startzeit und Endzeit festlegen
-        self.start = datetime(2023, 7, 15, 13, 0)
-        self.end = datetime(2023, 7, 26, 22, 0)
+        self.startdatum = datetime(2023, 7, 15, 13, 0)
+        self.enddatum = datetime(2023, 7, 26, 22, 0)
         self.delta = timedelta(hours=3, minutes=30)
+
+    def top_erfahrensten_segler(self, n=4):
+        self.crew = sorted(self.crew, key=lambda k: k['experience'], reverse=True)
+        return self.crew[:n]
+
+    def mindest_erfahrenen_segler(self, n=4):
+        self.crew.sort(key=lambda segler: segler['experience'])
+        return self.crew[:n]
+
+    def segler_mit_min_watch_count(self,wachpersonal):
+        return min(wachpersonal, key=lambda segler: segler['watch_count'])
 
     def erstellen_plan(self):
         # Leere Liste für Daten erstellen
         data = []
+        
+        # Diese Variable wird genutzt um startdatum weiter als eindeutige Variable zu haben
+        current_datum = self.startdatum
 
-        # Zuordnung der Wachen
-        while self.start <= self.end:
-            if len(self.expert) > 0:
-                expert1 = self.expert.pop(0)
-                if len(self.springer) > 0 and expert1 == "Detlef":
-                    unerf1 = self.unerfahren.pop(0)
-                    data.append([self.start, expert1, unerf1])
-                    self.unerfahren.append(unerf1)
-                else:
-                    spring1 = self.springer.pop(0)
-                    data.append([self.start, expert1, spring1])
-                    self.springer.append(spring1)
-                self.expert.append(expert1)
-            self.start += self.delta
+        # Iterriere von Startdatum bis Enddatum
+        while current_datum <= self.enddatum:
+            erfahrenes_wachpersonal=self.top_erfahrensten_segler()
+            wache1=self.segler_mit_min_watch_count(erfahrenes_wachpersonal)
+            unerfahrenes_wachpersonal=self.mindest_erfahrenen_segler()
+            wache2=self.segler_mit_min_watch_count(unerfahrenes_wachpersonal)
+            data.append([current_datum, wache1["name"], wache2["name"]])
+            wache1["watch_count"] += 1
+            wache2["watch_count"] += 1
+            current_datum += self.delta
 
         return data
 
@@ -39,8 +55,8 @@ class Schichtplaner:
         # DataFrame erstellen und als CSV speichern
         df = pd.DataFrame(data, columns=["Schichtbeginn", "Wachperson I", "Wachperson II"])
         df.to_csv("schichtplan.csv", index=False)
-
 if __name__ == "__main__":
     schichtplaner = Schichtplaner()
     data = schichtplaner.erstellen_plan()
     schichtplaner.speichern_csv(data)
+    print(schichtplaner.crew)
